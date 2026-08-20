769d8db1657d53a6fdbfe144c55991edcfb3b58a
Alex <iloverust228@gmail.com>
2026-08-20 04:26:19 +0300

    add some rviz stuff

--- src/chess_gz_wrld/launch/chess.launch.py
+++ src/chess_gz_wrld/launch/chess.launch.py
@@ -1,6 +1,8 @@
 from launch.actions import ExecuteProcess
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
@@ -9,6 +11,18 @@ def generate_launch_description():
     curr_pose = Node(
         package="chess_gz_wrld",
         executable="chess_tf",
+    )
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+    )
+    static_tf_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_tf_publisher",
+        arguments=["0", "0", "0", "0", "0", "0", "map", "chess_board"],
+        output="screen",
     )
 
     return LaunchDescription(
@@ -25,5 +39,7 @@ def generate_launch_description():
             ),
             gazebo,
             curr_pose,
+            rviz,
+            static_tf_node,
         ]
     )
